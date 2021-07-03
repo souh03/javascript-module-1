da808941d6c45a26c8ca433d61a32d588ed243da
said ouhmmou <84190116+souh03@users.noreply.github.com>
2021-07-03 10:45:45 +0200

    homework

---
+++ week-1/Homework/L-functions-nested/Exercise1 .js
@@ -0,0 +1,18 @@
+
+
+
+var totalnumber = 90;
+var numberOfStudents =60;
+var Studentspercentage = (numberOfStudents / totalnumber)*100 ; 
+var preciseStudentPercentage = Math.round(Studentspercentage);
+var Percentagestudents = "Percentage students:" + ( preciseStudentPercentage) + "%";
+console.log(Percentagestudents);
+
+var totalnumber = 90;
+var  numberOfMentors =30;
+var Mentorspercentage = (numberOfMentors / totalnumber)*100 ;
+var preciseMentorsPercentage = Math.round(Mentorspercentage);
+var Percentagementors = "Percentage mentors:"  +(preciseMentorsPercentage) + "%";
+console.log(Percentagementors);
+
+
