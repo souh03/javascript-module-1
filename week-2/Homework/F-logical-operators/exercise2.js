813f46b732ac914776566f1db9a685447036695f
said ouhmmou <84190116+souh03@users.noreply.github.com>
2021-09-18 13:48:30 +0200

    Add files via upload

--- week-2/Homework/F-logical-operators/exercise2.js
+++ week-2/Homework/F-logical-operators/exercise2.js
@@ -5,38 +5,43 @@
   Update the code so that you get the expected result.
 */
 
-function isNegative(number) {
-  if( number < 0 ){
-  return -10 <0;
+function isNegative(a) {
+  console.log(`Is ${a} a negative number?`);
+   return a<0;
 }
-else  {
-  return 5<0;
-  }
-}
+
+/* */
 function isBetween5and10(number){
-  if( number>= 5 && number<=10 ){
-    return number>= 5 && number<=10;
-  }
+  console.log(`Is ${number} in the range 5-10?`);
+  return number >= 5 && number <= 10;
 }
+
 function isShortName(name){
-  
-if (  name = "Daniel"){
-  return true ;
+  console.log(`Is ${name} a short name?`);
+   return name.length <=6;
 }
+
+function startsWithD1(string){
+  return string.charAt(0).toUpperCase() + string.slice(1);
 }
+console.log(startsWithD("daniel")); //returns Daniel
+
+
 function startsWithD(name){
-  if(name = "Daniel") {
-  return name.startsWith("D");
-}}
+  return name.charAt(0) === "D";
+}
+console.log(startsWithD("Daniel")); //returns true
+
+
 /* 
   DO NOT EDIT BELOW THIS LINE
   --------------------------- */
 
-console.log("Is -10 is a negative number?", isNegative(-10));
+/*console.log("Is -10 is a negative number?", isNegative(-10));
 console.log("Is 5 a negative number?", isNegative(5));
 console.log("Is 10 in the range 5-10?", isBetween5and10(10));
 console.log("Is Daniel a short name?", isShortName("Daniel"));
-console.log("Does Daniel start with 'D'?", startsWithD("Daniel"));
+console.log("Does Daniel start with 'D'?", startsWithD("Daniel")); */
 
 /* 
   EXPECTED RESULT
@@ -45,5 +50,5 @@ console.log("Does Daniel start with 'D'?", startsWithD("Daniel"));
   Is 5 a negative number? false
   Is 10 in the range 5-10? true
   Is Daniel a short name? true
-  Does Daniel start with 'D'? true
+  Does Daniel start with 'D'?
 */
