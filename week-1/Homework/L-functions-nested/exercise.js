f5fb4254f428aab96d957871244b3189d158d8f1
said ouhmmou <84190116+souh03@users.noreply.github.com>
2021-09-18 13:37:38 +0200

    Add files via upload

--- week-1/Homework/L-functions-nested/exercise.js
+++ week-1/Homework/L-functions-nested/exercise.js
@@ -1,5 +1,26 @@
+
 var mentor1 = "Daniel";
 var mentor2 = "Irina";
 var mentor3 = "Mimi";
 var mentor4 = "Rob";
 var mentor5 = "Yohannes";
+
+
+
+function  mentorName(greeting){
+    var Uppercaste = greeting.mentorNamecase();
+    return Uppercaste;
+}
+function greetingMentor( name, greetingstart){
+    var greeting = greetingstart + " " + name;
+    var uppergreting = mentorName(greeting);
+    return uppergreting;
+}
+
+var  greetingstart = "Hello"; 
+
+console.log(greetingMentor(mentor1,greetingstart));
+console.log(greetingMentor(mentor2,greetingstart));
+console.log(greetingMentor(mentor3,greetingstart));
+console.log(greetingMentor(mentor4,greetingstart));
+console.log(greetingMentor(mentor5,greetingstart));
